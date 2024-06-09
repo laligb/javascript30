7ee3819e3e0e04f0268b5c1031acfad4a3f41f60
Lali Bibilashvili <lalibibilashvili@gmail.com>
2024-06-10 01:44:30 +0200

    finished

--- Day1-Drum-Kit/main.js
+++ Day1-Drum-Kit/main.js
@@ -1,3 +1,17 @@
-window.addEventListener("keydown", function (e) {
-  console.log(e); // Checking how if eventlistener appears
-});
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play(); // play audio
+  key.classList.add("playing"); // adding class 'playing'
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return; // Skip it if it is not transform
+  this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+window.addEventListener("keydown", playSound);
